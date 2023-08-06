6ad52b3bf6ed7db976da9c293c17813378dc2984
Ngurudev <Ngurudeveloper@gmail.com>
2023-08-06 20:25:12 +0100

    irs

---
+++ templates/frontend/sidebar.php
@@ -0,0 +1,57 @@
+<aside>
+    <div id="sidebar" class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu" id="nav-accordion">
+            <p class="centered"><a href="profile.html"><img src="img/ui-sam.jpg" class="img-circle" width="80"></a></p>
+            <h5 class="centered">Sam Soffes</h5>
+            <li class="mt">
+                <a class="active" href="index.php">
+                    <i class="fa fa-dashboard"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;">
+                    <i class="fa fa-users"></i>
+                    <span>Admins</span>
+                </a>
+                <ul class="sub">
+                    <li><a href="add_admin.php">Add Admins</a></li>
+                    <li><a href="manage_admins.php">Manage Admins</a></li>
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;">
+                    <i class="fa fa-users"></i>
+                    <span>Users</span>
+                </a>
+                <ul class="sub">
+                    <li><a href="calendar.html">Add User</a></li>
+                    <li><a href="manage_users.php">Manage Users</a></li>
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;">
+                    <i class="fa fa-book"></i>
+                    <span>Categories</span>
+                </a>
+                <ul class="sub">
+                    <li><a href="blank.html">Add Category</a></li>
+                    <li><a href="login.html">Manage Categories</a></li>
+                    <li><a href="404.html">404 Error</a></li>
+                    <li><a href="500.html">500 Error</a></li>
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;">
+                    <i class="fa fa-money"></i>
+                    <span>Payments</span>
+                </a>
+                <ul class="sub">
+                    <li><a href="payment_history.php">Payments History</a></li>
+                </ul>
+            </li>
+        </ul>
+        <!-- sidebar menu end-->
+    </div>
+</aside>
